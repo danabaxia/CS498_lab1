0baf4e177e4ce9e4852c698da913dde03de1fd22
danabaxia <danabaxia@gmail.com>
2021-09-10 02:17:20 -0500

    Update part1_obstacle_avoidance.py
    improve the speed at no obstacle
    -JB

--- part1_obstacle_avoidance.py
+++ part1_obstacle_avoidance.py
@@ -14,15 +14,19 @@ def StopAndPickNewDirection(scan_list):
     right = scan_list[5:]
     if left.count(2) > right.count(2) : 
         fc.turn_left(50)
+        print("turn left")
     elif left.count(2) < right.count(2) :
         fc.turn_right(50)
+        print("turn right")
     else :
         turnRight = random.randint(0,1)
         if(turnRight):
             fc.turn_right(50)
         else:
             fc.turn_left(50)
-    turnTime = random.random() + 0.1
+        print("random direction")
+    #turnTime = random.random() + 0.1
+    turnTime = 0.5
     time.sleep(turnTime)
     fc.stop()
 
@@ -35,7 +39,11 @@ def ObstacleAvoidance():
             continue
 
         print(scanList)
-        if scanList[3:7] != [2,2,2,2]:
+        if scanList.count(0) > 0:
+            fc.backward(20)
+            time.sleep(0.5)
+            continue
+        elif scanList[3:7] != [2,2,2,2]:
             StopAndPickNewDirection(scanList)
             power = 0
         
@@ -43,6 +51,8 @@ def ObstacleAvoidance():
         power += 5
         if(power >= maxPower):
             power = maxPower
+        if scanList.count(2) == 10:
+            power = 40
         print(power)
         fc.forward(power)
         
